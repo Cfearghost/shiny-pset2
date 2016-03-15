b353bf2461ebf19961544351a80fa958185faaf0
Christi Balaki <cbalaki@college.harvard.edu>
2016-03-15 16:10:04 -0400

    "file"

---
+++ matrix.py
@@ -0,0 +1,13 @@
+def matrixproduct(A, B):
+    n = len(A)
+    C = [[0 for i in xrange(n)] for j in xrange(n)]
+    for i in xrange(n):
+        for k in xrange(n):
+            for j in xrange(n):
+                C[i][j] += A[i][k] * B[k][j]
+    return C
+
+A = [[1,1,1],[1,1,1],[1,1,1]]
+B = [[1,1,1],[1,1,1],[1,1,1]]
+C = matrixproduct(A, B)
+printMatrix(C)
