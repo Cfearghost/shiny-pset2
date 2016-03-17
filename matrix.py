fff352503d5af65c4d5cdd0321c1fee83954151e
Christi Balaki <cbalaki@college.harvard.edu>
2016-03-17 16:32:05 -0400

    "Commiting times"

--- matrix.py
+++ matrix.py
@@ -1,3 +1,8 @@
+import time
+
+# Could put all answers of normal in on array, then another
+# run through whole array
+
 def printMatrix(matrix):
     for line in matrix:
         print "\t".join(map(str,line))
@@ -11,7 +16,23 @@ def matrixproduct(A, B):
                 C[i][j] += A[i][k] * B[k][j]
     return C
 
+
+
+start_time = time.time()
 A = [[1,1,1],[1,1,1],[1,1,1]]
 B = [[1,1,1],[1,1,1],[1,1,1]]
 C = matrixproduct(A, B)
 printMatrix(C)
+end = time.time()
+print("%s seconds" % (time.time() - start_time))
+start_time = time.time()
+C = matrixproduct(A, B)
+C = matrixproduct(A, B)
+C = matrixproduct(A, B)
+C = matrixproduct(A, B)
+C = matrixproduct(A, B)
+end = time.time()
+print("%s seconds" % (time.time() - start_time))
+
+
+
